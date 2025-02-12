866620f021314fce183fa7054728358bf72e76a9
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2025-02-12 14:14:21 +0900

    [FEAT] 버전 표시 기능 구현

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
+    public TMP_Text versionText;
+
     void Start()
     {
-        
+        versionText.text = Application.version;
     }
 
     public void Play( )
